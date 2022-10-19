9b21227fbddf80358cf41e89795a4dfb9902432b
Ephraimf10 <ephriam_fred@yahoo.com>
2022-10-19 09:11:27 +0000

    Nested loops

---
+++ 0x02-funtions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints the putchar
+ *
+ * Return: Zero Value if successful
+ */
+int main(void)
+{
+	char fred[] = "_putchar";
+	int i = 0;
+
+	while (fred[i] != '\0')
+	{
+		_putchar(uju[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
